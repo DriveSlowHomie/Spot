0be3ab1086923f805e98c0327e98e6fe3143ec18
Sagar Samantarai <holysagar@gmail.com>
2016-08-16 10:49:14 -0500

    Quick Fix

--- ngApp/js/controllers.ts
+++ ngApp/js/controllers.ts
@@ -28,6 +28,7 @@ namespace SpotApp.Controllers {
                 private $state: ng.ui.IStateService
     ){}
   }
+
   angular.module('SpotApp').controller('HomeController', HomeController);
 
 
@@ -40,45 +41,47 @@ namespace SpotApp.Controllers {
        private $uibModalInstance: angular.ui.bootstrap.IModalServiceInstance){
     }
   }
+
   angular.module('SpotApp').controller('DialogController', DialogController);
 
 
 
   export class MarkPageController {
-    public coords;
-    public myLocation(){}
+    // public coords;
+    // public myLocation(){}
 
     public center = { latitude: 37.09024, longitude: -100.712891};
     public zoom = 4;
 
-    public add() {
-    let params = {
-      geolocation: this.coords.coords
-      // title: this.movie.title,
-      // genre: this.movie.genre
-    };
-
-    this.geolocationService.create(params).then((res) => {});
-    }
-
+    // public add() {
+    // let params = {
+    //   geolocation: this.coords.coords
+    //  // title: this.movie.title,
+    //  // genre: this.movie.genre
+    // };
+    //
+    // this.geolocationService.create(params).then((res) => {});
+    // }
+    //
 
     constructor(
-      $geolocation,
-      private geolocationService: SpotApp.Services.GeolocationService,
-      private $state: ng.ui.IStateService
+      // $geolocation,
+      // private geolocationService: SpotApp.Services.GeolocationService,
+      // private $state: ng.ui.IStateService
     ) {
-      $geolocation.getCurrentPosition({
-        timeout: 60000
-      }).then(function(position){
-        this.coords = {
-          lat: parseFloat(position.coords.latitude),
-          lng: parseFloat(position.coords.latitude)
-        }
-        console.log(this.coords);
-      })
+      // $geolocation.getCurrentPosition({
+      //   timeout: 60000
+      // }).then(function(position){
+      //   this.coords = {
+      //     lat: parseFloat(position.coords.latitude),
+      //     lng: parseFloat(position.coords.latitude)
+      //   }
+      //   console.log(this.coords);
+      // })
 
     }
   }
+    angular.module('SpotApp').controller('MarkPageController', MarkPageController);
 
 
   export class ExplorePageController {
@@ -97,108 +100,3 @@ namespace SpotApp.Controllers {
   }
 
 }
-
-
-
-
-// namespace atl.Controllers {
-//   export class HomeController {
-//       // public movies;
-//
-//     constructor(/*private movieService: atl.Services.MovieService*/) {
-//       // this.movies = this.movieService.getAll();
-//     }
-//   }
-//
-//   export class addController {
-//     // public movie;
-//     constructor(
-//       // private movieService: atl.Services.MovieService,
-//       // private $state: ng.ui.IStateService
-//     ) {
-//
-//     }
-//
-//     // public save() {
-//     //   let params = {
-//     //     title: this.movie.title,
-//     //     genre: this.movie.genre
-//     //   }
-//     //
-//     //   this.movieService.save(params).then((res) => {
-//     //     this.$state.go("Home");
-//     //   })
-//     // }
-//
-//   }
-//
-//   angular.module('SpotApp').controller('HomeController', HomeController);
-// }
-
-
-
-// namespace SpotApp.Controllers {
-//
-//   export class HomeController {
-//     public email;
-//     showModal(){
-//       this.$uibModal.open({
-//         templateUrl: '/templates/dialog.html',
-//         controller: 'DialogController',
-//         controllerAs: 'vm',
-//         resolve: {
-//           dataFromHomeController: () => this.email
-//         },
-//         size: 'sm'
-//       });
-//     }
-//     constructor(private $uibModal: angular.ui.bootstrap.IModalService) {
-//     }
-//   }
-//   angular.module('SpotApp').controller('HomeController', HomeController);
-//
-//
-//
-//
-//   export class MarkPageController {
-//     public coords;
-//     public myLocation(){
-//     }
-//
-//     public center = { latitude: 37.09024, longitude: -100.712891};
-//     public zoom = 4;
-//
-//
-//
-//
-//     constructor($geolocation) {
-//       $geolocation.getCurrentPosition({
-//         timeout: 60000
-//       }).then(function(position) {
-//         this.coords = {
-//           lat: parseFloat(position.coords.latitude),
-//           lng: parseFloat(position.coords.latitude)
-//         }
-//         console.log(this.coords);
-//       })
-//
-//     }
-//   }
-//
-//
-//   export class ExplorePageController {
-//
-//     constructor() {
-//
-//     }
-//   }
-//
-//   export class HitPageController {
-//
-//
-//     constructor() {
-//
-//     }
-//   }
-//
-// }
